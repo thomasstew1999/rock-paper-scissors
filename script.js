d9ad97762e5bcabe866a0a51c726e456050824d1
thomasstew1999 <thomasstew1999@gmail.com>
2025-04-05 21:31:10 -0500

    Finished

--- script.js
+++ script.js
@@ -29,43 +29,44 @@ function rollRPS(choice) {
 }
 
 function playRound(humanRoll, aiRoll) {
-    if (score < 4 || aiScore < 4) {
+    if ((score != 5 && aiScore != 5)) {
         if (humanRoll == aiRoll) {
             document.getElementById('ptext').textContent = "You tied this round";
         } else if (humanRoll == 0 && aiRoll == 2) {
-            document.getElementById('ptext').textContent = "You won this round";
+            document.getElementById('ptext').textContent = "Rock beats scissors";
             score++;
             document.getElementById('score').textContent = `${score}`;
         } else if (humanRoll == 1 && aiRoll == 0) {
-            document.getElementById('ptext').textContent = "You won this round";
+            document.getElementById('ptext').textContent = "Paper beats rock";
             score++;
             document.getElementById('score').textContent = `${score}`;
         } else if (humanRoll == 2 && aiRoll == 1) {
-            document.getElementById('ptext').textContent = "You won this round";
+            document.getElementById('ptext').textContent = "Scissors beats paper";
             score++;
             document.getElementById('score').textContent = `${score}`;
         } else if (humanRoll == 2 && aiRoll == 0) {
-            document.getElementById('ptext').textContent = "You lost this round";
+            document.getElementById('ptext').textContent = "Scissors lost to rock";
             aiScore++;
             document.getElementById('ai-score').textContent = `${aiScore}`;
         } else if (humanRoll == 0 && aiRoll == 1) {
-            document.getElementById('ptext').textContent = "You lost this round";
+            document.getElementById('ptext').textContent = "Rock lost to paper";
             aiScore++;
             document.getElementById('ai-score').textContent = `${aiScore}`;
         } else if (humanRoll == 1 && aiRoll == 2) {
-            document.getElementById('ptext').textContent = "You lost this round";
+            document.getElementById('ptext').textContent = "Paper lost to scissors";
             aiScore++;
             document.getElementById('ai-score').textContent = `${aiScore}`;
         }
-    } else {
-        if (score == 5) {
-            document.getElementById('ptext').textContent = "You won the game!";
-        } else if (aiScore == 5) {
-            document.getElementById('ptext').textContent = "You lost the game!";
+        if (score == 5 || aiScore == 5) {
+            if (score == 5) {
+                document.getElementById('ptext').textContent = "You won the game!";
+            } else if (aiScore == 5) {
+                document.getElementById('ptext').textContent = "You lost the game!";
+            }
+            score = 0;
+            aiScore = 0;
+            document.getElementById('ai-score').textContent = `${aiScore}`
+            document.getElementById('score').textContent = `${score}`;
         }
-        score = 0;
-        aiScore = 0;
-        document.getElementById('ai-score').textContent = `${aiScore}`
-        document.getElementById('score').textContent = `${score}`;
     }
 }
