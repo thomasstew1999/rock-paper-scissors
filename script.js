ad73e850e3aef9a9429b135596862bc775f80726
thomasstew1999 <thomasstew1999@gmail.com>
2025-04-05 21:03:52 -0500

    Basic functionality down

---
+++ script.js
@@ -0,0 +1,63 @@
+let roll;
+let aiRoll;
+let aiChoice;
+let score = 0;
+let aiScore = 0;
+
+document.getElementById('rock').addEventListener("click", function () {
+    rollRPS(0);
+});
+document.getElementById('paper').addEventListener("click", function () {
+    rollRPS(1);
+});
+document.getElementById('scissors').addEventListener("click", function () {
+    rollRPS(2);
+});
+
+
+function rollRPS(choice) {
+    roll = choice;
+    aiChoice = Math.random();
+    if (aiChoice >= 0 && aiChoice <= .33) {
+        aiRoll = 0;
+    } else if (aiChoice > .33 && aiChoice <= .66) {
+        aiRoll = 1;
+    } else {
+        aiRoll = 2;
+    }
+    playRound(roll, aiRoll);
+}
+
+function playRound(humanRoll, aiRoll) {
+    if (score < 5 || aiScore < 5) {
+        if (humanRoll == aiRoll) {
+            document.getElementById('ptext').textContent = "You tied this round";
+        } else if (humanRoll == 0 && aiRoll == 2) {
+            document.getElementById('ptext').textContent = "You won this round";
+            score++;
+        } else if (humanRoll == 1 && aiRoll == 0) {
+            document.getElementById('ptext').textContent = "You won this round";
+            score++;
+        } else if (humanRoll == 2 && aiRoll == 1) {
+            document.getElementById('ptext').textContent = "You won this round";
+            score++;
+        } else if (humanRoll == 2 && aiRoll == 0) {
+            document.getElementById('ptext').textContent = "You lost this round";
+            aiScore++;
+        } else if (humanRoll == 0 && aiRoll == 1) {
+            document.getElementById('ptext').textContent = "You lost this round";
+            aiScore++;
+        } else if (humanRoll == 1 && aiRoll == 2) {
+            document.getElementById('ptext').textContent = "You lost this round";
+            aiScore++;
+        }
+    } else {
+        if (roll == 5) {
+            document.getElementById('ptext').textContent = "You won the game!";
+        } else{
+            document.getElementById('ptext').textContent = "You lost the game!";
+        }
+        score = 0;
+        aiScore = 0;
+    }
+}
